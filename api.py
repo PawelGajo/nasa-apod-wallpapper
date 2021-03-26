85465ecf4174d585e5d85a6553456e0b9606b26d
gpawel1997@gmail.com <gpawel1997@gmail.com>
2021-03-26 23:47:22 +0100

    add function for downloading todays apod and random apod.

--- api.py
+++ api.py
@@ -2,29 +2,50 @@ import requests
 from pathlib import Path
 import shutil
 
+
 class NasaApi():
     def __init__(self, key='DEMO_KEY'):
         self.api_key = key
 
-    def getApod(self):
-        return requests.get('https://api.nasa.gov/planetary/apod?api_key=' + self.api_key)
+    def getTodayApodUrl(self):
+        return requests.get('https://api.nasa.gov/planetary/apod?api_key=' + self.api_key).json()['url']
 
-    def getApodUrl(self):
-        return self.getApod().json()['url']
-    
-    
-    def getApodHdUrl(self):
-        return self.getApod().json()['hdurl']
+    def getTodayApodHdUrl(self):
+        return requests.get('https://api.nasa.gov/planetary/apod?api_key=' + self.api_key).json()['hdurl']
 
-    def downloadApod(self, destinationPath = None):
-        if destinationPath == None:
-            destinationPath = Path().absolute()
+    def getRandomApodUrl(self):
+        return requests.get('https://api.nasa.gov/planetary/apod?count=1&api_key=' + self.api_key).json()[0]['url']
 
-        apodUrl = self.getApodHdUrl()
-        r = requests.get(apodUrl, stream=True)
+    def getRandomApodHdUrl(self):
+        return requests.get('https://api.nasa.gov/planetary/apod?count=1&api_key=' + self.api_key).json()[0]['hdurl']
+
+    def downloadFile(self, source, destinationPath):
+        r = requests.get(source, stream=True)
         if r.status_code == 200:
             with open(destinationPath, 'wb') as f:
                 r.raw.decode_content = True
-                shutil.copyfileobj(r.raw, f) 
+                shutil.copyfileobj(r.raw, f)
             return True
-        return False  
+        return False
+
+    def downloadTodaysApod(self, destinationPath=None, hd=True):
+        if destinationPath == None:
+            destinationPath = Path().absolute() / 'apod.jpg'
+
+        if hd:
+            apodUrl = self.getTodayApodHdUrl()
+        else:
+            apodUrl = self.getTodayApodUrl()
+
+        return self.downloadFile(apodUrl, destinationPath)
+
+    def downloadRandomApod(self, destinationPath=None, hd=True):
+        if destinationPath == None:
+            destinationPath = Path().absolute() / 'apod.jpg'
+
+        if hd:
+            apodUrl = self.getRandomApodHdUrl()
+        else:
+            apodUrl = self.getRandomApodUrl()
+
+        return self.downloadFile(apodUrl, destinationPath)

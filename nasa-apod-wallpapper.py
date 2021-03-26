fce2defdd2d71f4ee62ca09c5d72835d40b8c780
gpawel1997@gmail.com <gpawel1997@gmail.com>
2021-03-27 00:00:07 +0100

    update main file

--- nasa-apod-wallpapper.py
+++ nasa-apod-wallpapper.py
@@ -1,10 +1,19 @@
-from api import NasaApi
+from nasaapi import NasaApi
+from utils import setWallpaper
 from pathlib import Path
 
 
+# destination path with name for downloaded img
+imgDestinationPath = Path().absolute() / 'images' / 'apod.jpg'
 
-#set your API key from https://api.nasa.gov/ . Its DEMO_KEY by default
+# #set your API key from https://api.nasa.gov/ . Its DEMO_KEY by default
 nasaApi = NasaApi()
 
+# ---- download today's apod and save in destination path. hd parameter defines img resolution.
+# nasaApi.downloadTodaysApod(destinationPath=imgDestinationPath)
 
-print(nasaApi.downloadApod(destinationPath=Path().absolute() / 'images' / 'today.jpg' ))
+# ---- download random apod img and save in destination path. hd parameter defines img resolution.
+nasaApi.downloadRandomApod(destinationPath=imgDestinationPath)
+
+# set img as wallpaper
+setWallpaper(imgDestinationPath)
